754181a720e423df4db5de744a4e8b6880b37c69
xssxx <koonx6520@gmail.com>
2024-02-23 22:04:26 +0700

    train seperate type, apple (skull)

--- helpers/window.py
+++ helpers/window.py
@@ -1,13 +1,19 @@
-def window_input_output(inp, out, data):
+import pandas as pd
+
+def window_input_output(inp: int, out: int, data: pd.DataFrame) -> pd.DataFrame:
     df = data.copy()
 
     # Create input columns
-    for i in range(1, inp):
-        df[f'x_{i}'] = df.iloc[:, 0].shift(-i)
+    i = 1
+    while i < inp:
+        df[f'x_{i}'] = df.iloc[:, 0].shift(i)
+        i += 1
 
     # Create output columns
-    for j in range(0, out):
-        df[f'y_{j}'] = df.iloc[:, 0].shift(-out + j)
+    j = 0
+    while j < out:
+        df[f'y_{j}'] = df.iloc[:, 0].shift(out+j)
+        j += 1
 
     # Drop rows with NaN values
     df = df.dropna(axis=0)
